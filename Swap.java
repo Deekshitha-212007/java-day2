d1555e9862e50c1ab33702afab400d58739d93d7
Deekshitha-212007 <nagamahe2007@gmail.com>
2026-02-07 15:19:18 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,14 @@
+public class Swap{
+    public static void main(String[] args) {
+        int a=3;
+        int b=6;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        a=a*b;
+        b=a/b;
+        a=a/b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
