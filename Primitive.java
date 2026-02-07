82a92bae090c1f50640bca69a378de1317fca7b2
Deekshitha-212007 <nagamahe2007@gmail.com>
2026-02-07 14:17:42 +0530

    Add files via upload

---
+++ Primitive.java
@@ -0,0 +1,12 @@
+public  class Primitive{
+    public static void main(String[] args) {
+        int hi=85;
+        int eng=96;
+        int mat=95;
+        int sum=(hi+eng+mat);
+        int avg=sum/3;
+        System.out.println(sum);
+        System.err.println(avg);
+        
+    }
+}
