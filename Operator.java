3e14d2072ec43ec439e4e064202906a20e97ee87
Deekshitha-212007 <nagamahe2007@gmail.com>
2026-02-07 14:16:13 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,29 @@
+public class Operator{
+    public static void main(String[]args)
+    {
+        int a=13;
+        int b=56;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println("Relational Operator");
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        System.out.println("Unary Operator");
+        System.out.println(a--);
+        System.out.println(++b);
+        System.out.println(--a);
+        System.out.println(b++);
+        System.out.println(-a);
+        System.out.println(+b);
+        System.out.println(-b);
+        System.out.println(+a);
+
+    }
+}
