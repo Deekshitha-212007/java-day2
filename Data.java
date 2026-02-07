956374a41eb85434369e1d9cce4b6d3dba9561b0
Deekshitha-212007 <nagamahe2007@gmail.com>
2026-02-07 14:17:13 +0530

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,20 @@
+public class Data{
+    public static void main(String[]args)
+    {
+        String studentname="kavya";
+        int age=18;
+        char grade='A';
+        int marks=98;
+        System.out.println("studentname:kavya");
+        System.out.println("age:18");
+        System.out.println("grade:A");
+        System.out.println("marks:98");
+        if (98<=35) 
+        {
+            System.out.println("pass");
+        }
+        else{
+            System.out.println("fail");
+        }
+    }
+}
